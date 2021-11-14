1c662c8f4024eb60e2ec672f7b6afc73662fbdea
Mark Godwin <10632972+MarkGodwin@users.noreply.github.com>
2021-11-14 13:38:48 +0000

    New light colour and cover position support

--- homeassistant/components/scene_state/switch.py
+++ homeassistant/components/scene_state/switch.py
@@ -1,33 +1,41 @@
 """Platform for scene state switch integration."""
-from typing import Any, List, Optional
-from homeassistant.const import (
-    TEMP_CELSIUS,
-    EVENT_STATE_CHANGED,
-    SERVICE_TURN_ON,
-    ATTR_ENTITY_ID,
+import logging
+from typing import List, Optional
+
+from homeassistant.components.cover import (
+    ATTR_CURRENT_POSITION,
+    ATTR_CURRENT_TILT_POSITION,
+    SUPPORT_SET_POSITION,
+    SUPPORT_SET_TILT_POSITION,
 )
-from homeassistant.helpers.entity import Entity
-from homeassistant.components.switch import SwitchEntity
 from homeassistant.components.light import (
-    SUPPORT_BRIGHTNESS,
-    SUPPORT_COLOR_TEMP,
-    SUPPORT_EFFECT,
-    SUPPORT_COLOR,
-    SUPPORT_TRANSITION,
-    SUPPORT_WHITE_VALUE,
     ATTR_BRIGHTNESS,
     ATTR_COLOR_TEMP,
     ATTR_RGB_COLOR,
-    ATTR_WHITE_VALUE,
+    ATTR_RGBW_COLOR,
+    ATTR_RGBWW_COLOR,
+    ATTR_SUPPORTED_COLOR_MODES,
+    COLOR_MODE_BRIGHTNESS,
+    COLOR_MODE_COLOR_TEMP,
+    COLOR_MODE_RGB,
+    COLOR_MODE_RGBW,
+    COLOR_MODE_RGBWW,
 )
-import logging
+from homeassistant.components.switch import SwitchEntity
+from homeassistant.const import (
+    ATTR_ENTITY_ID,
+    ATTR_SUPPORTED_FEATURES,
+    EVENT_STATE_CHANGED,
+    SERVICE_TURN_ON,
+)
 
 SCENE_DATA_PLATFORM = "homeassistant_scene"
 DATA_PLATFORM = "switch"
-_LOGGER = logging.getLogger("scene_state")
+_LOGGER = logging.getLogger(__name__)
 
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
+    """Set up the scene switch platform."""
 
     # Get the scenes registered with HA
     if SCENE_DATA_PLATFORM not in hass.data:
@@ -110,6 +118,7 @@ class SceneStateSwitch(SwitchEntity):
         return self._icon
 
     def get_tracked_entities(self) -> List[str]:
+        """Get the entities which are tracked by the scene."""
         if self.hass is None:
             return []
         scenePlatform = self.hass.data[SCENE_DATA_PLATFORM]
@@ -118,34 +127,114 @@ class SceneStateSwitch(SwitchEntity):
         scene = scenePlatform.entities.get(self._sceneId)
         if scene is None:
             return []
-        return [entity_id for entity_id in scene.scene_config.states]
+        return list(scene.scene_config.states)
 
     def _compare_simple_state(self, sceneState) -> bool:
-        return self.hass.states.get(sceneState.entity_id).state == sceneState.state
+        currentState = self.hass.states.get(sceneState.entity_id)
+        return currentState is not None and currentState.state == sceneState.state
 
     def _compare_light_state(self, sceneState) -> bool:
+        _LOGGER.debug(
+            "%s - Comparing light state for %s(%s)",
+            self.name,
+            sceneState.name,
+            sceneState.entity_id,
+        )
         currentState = self.hass.states.get(sceneState.entity_id)
+        if currentState is None:
+            return False
         if currentState.state != sceneState.state:
+            _LOGGER.debug(
+                "State does not match: %s != %s", currentState.state, sceneState.state
+            )
             return False
 
         if currentState.state == "off":
-            # Off is off.
+            # Off is off, regardless of colour
+            _LOGGER.debug("State matches off")
             return True
 
         # Compare relevant attributes
-        supported_features = currentState.attributes.get("supported_features", 0)
+        supported_color_modes = currentState.attributes.get(
+            ATTR_SUPPORTED_COLOR_MODES, []
+        )
 
-        attrs_to_check = {
-            SUPPORT_BRIGHTNESS: ATTR_BRIGHTNESS,
-            SUPPORT_COLOR_TEMP: ATTR_COLOR_TEMP,
-            SUPPORT_COLOR: ATTR_RGB_COLOR,
-            SUPPORT_WHITE_VALUE: ATTR_WHITE_VALUE,
+        fuzzyAttrs = {
+            COLOR_MODE_BRIGHTNESS: ATTR_BRIGHTNESS,
+            COLOR_MODE_COLOR_TEMP: ATTR_COLOR_TEMP,
         }
 
-        for key in attrs_to_check.keys():
-            attr = attrs_to_check[key]
+        listAttrs = {
+            COLOR_MODE_RGB: ATTR_RGB_COLOR,
+            COLOR_MODE_RGBW: ATTR_RGBW_COLOR,
+            COLOR_MODE_RGBWW: ATTR_RGBWW_COLOR,
+        }
 
-            if supported_features & key:
+        for key in supported_color_modes:
+            attr = fuzzyAttrs.get(key)
+
+            if attr is not None:
+                _LOGGER.debug(
+                    "%s Comparing %s for %s", self.name, attr, sceneState.name
+                )
+                _LOGGER.debug(
+                    "Current %s: %d, Scene state: %d",
+                    attr,
+                    currentState.attributes.get(attr),
+                    sceneState.attributes.get(attr),
+                )
+                if (
+                    abs(
+                        currentState.attributes.get(attr)
+                        - sceneState.attributes.get(attr)
+                    )
+                    > 3
+                ):
+                    return False
+            else:
+                attr = listAttrs.get(key)
+                if attr is not None and currentState.attributes.get(
+                    attr
+                ) != sceneState.attributes.get(attr):
+                    return False
+
+        _LOGGER.debug("All supported colour mode attributes match")
+        return True
+
+    def _compare_cover_state(self, sceneState) -> bool:
+        _LOGGER.debug(
+            "%s - Comparing cover state for %s(%s)",
+            self.name,
+            sceneState.name,
+            sceneState.entity_id,
+        )
+        currentState = self.hass.states.get(sceneState.entity_id)
+        if currentState is None:
+            return False
+        if currentState.state != sceneState.state:
+            _LOGGER.debug(
+                "Top-level state does not match: %s != %s",
+                currentState.state,
+                sceneState.state,
+            )
+            return False
+
+        if currentState.state == "closed":
+            # closed is closed, regardless of position
+            _LOGGER.debug("State matches closed")
+            return True
+
+        # Compare relevant attributes
+        supported_features = currentState.attributes.get(ATTR_SUPPORTED_FEATURES, 0)
+        fuzzyAttrs = {
+            SUPPORT_SET_POSITION: ATTR_CURRENT_POSITION,
+            SUPPORT_SET_TILT_POSITION: ATTR_CURRENT_TILT_POSITION,
+        }
+
+        for key in fuzzyAttrs.keys():
+            if supported_features & key > 0:
+                attr = fuzzyAttrs[key]
+
                 _LOGGER.debug(
                     "%s Comparing %s for %s", self.name, attr, sceneState.name
                 )
@@ -164,17 +253,17 @@ class SceneStateSwitch(SwitchEntity):
                 ):
                     return False
 
+        _LOGGER.debug("All supported cover position attributes match")
         return True
 
     def update(self):
-        """
-        Compare the current entity state to the scene state
-        """
+        """Compare the current entity state to the scene state."""
         scenePlatform = self.hass.data[SCENE_DATA_PLATFORM]
         scene = scenePlatform.entities.get(self._sceneId)
 
         switch = {
             "light": self._compare_light_state,
+            "cover": self._compare_cover_state,
         }
 
         for sceneState in scene.scene_config.states.values():
