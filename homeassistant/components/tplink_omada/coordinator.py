81f6fabd6c190cd008c4be802bb7c90da704f248
MarkGodwin <10632972+MarkGodwin@users.noreply.github.com>
2024-04-14 21:35:35 +0000

    Add device trackers to tplink_omada

--- homeassistant/components/tplink_omada/coordinator.py
+++ homeassistant/components/tplink_omada/coordinator.py
@@ -5,7 +5,9 @@ from datetime import timedelta
 import logging
 from typing import Generic, TypeVar
 
-from tplink_omada_client import OmadaSiteClient
+from tplink_omada_client import OmadaSiteClient, OmadaSwitchPortDetails
+from tplink_omada_client.clients import OmadaWirelessClient
+from tplink_omada_client.devices import OmadaGateway, OmadaSwitch
 from tplink_omada_client.exceptions import OmadaClientException
 
 from homeassistant.core import HomeAssistant
@@ -14,6 +16,10 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator, Upda
 _LOGGER = logging.getLogger(__name__)
 
 T = TypeVar("T")
+
+POLL_SWITCH_PORT = 300
+POLL_GATEWAY = 300
+POLL_CLIENTS = 300
 
 
 class OmadaCoordinator(DataUpdateCoordinator[dict[str, T]], Generic[T]):
@@ -46,3 +52,59 @@ class OmadaCoordinator(DataUpdateCoordinator[dict[str, T]], Generic[T]):
     async def poll_update(self) -> dict[str, T]:
         """Poll the current data from the controller."""
         raise NotImplementedError("Update method not implemented")
+
+
+class OmadaSwitchPortCoordinator(OmadaCoordinator[OmadaSwitchPortDetails]):
+    """Coordinator for getting details about ports on a switch."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        omada_client: OmadaSiteClient,
+        network_switch: OmadaSwitch,
+    ) -> None:
+        """Initialize my coordinator."""
+        super().__init__(
+            hass, omada_client, f"{network_switch.name} Ports", POLL_SWITCH_PORT
+        )
+        self._network_switch = network_switch
+
+    async def poll_update(self) -> dict[str, OmadaSwitchPortDetails]:
+        """Poll a switch's current state."""
+        ports = await self.omada_client.get_switch_ports(self._network_switch)
+        return {p.port_id: p for p in ports}
+
+
+class OmadaGatewayCoordinator(OmadaCoordinator[OmadaGateway]):
+    """Coordinator for getting details about the site's gateway."""
+
+    def __init__(
+        self,
+        hass: HomeAssistant,
+        omada_client: OmadaSiteClient,
+        mac: str,
+    ) -> None:
+        """Initialize my coordinator."""
+        super().__init__(hass, omada_client, "Gateway", POLL_GATEWAY)
+        self.mac = mac
+
+    async def poll_update(self) -> dict[str, OmadaGateway]:
+        """Poll a the gateway's current state."""
+        gateway = await self.omada_client.get_gateway(self.mac)
+        return {self.mac: gateway}
+
+
+class OmadaClientsCoordinator(OmadaCoordinator[OmadaWirelessClient]):
+    """Coordinator for gettings details about the site's connected clients."""
+
+    def __init__(self, hass: HomeAssistant, omada_client: OmadaSiteClient) -> None:
+        """Initialize my coordinator."""
+        super().__init__(hass, omada_client, "ClientsList", POLL_CLIENTS)
+
+    async def poll_update(self) -> dict[str, OmadaWirelessClient]:
+        """Poll the site's current active wi-fi clients."""
+        return {
+            c.mac: c
+            async for c in self.omada_client.get_connected_clients()
+            if isinstance(c, OmadaWirelessClient)
+        }
